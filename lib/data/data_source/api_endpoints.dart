9cd7f87bef6e122526f16243911783d6de402378
Amit_Aditaya <amit.aditaya99@gmail.com>
2025-01-30 11:47:08 +0600

    base url updated

--- lib/data/data_source/api_endpoints.dart
+++ lib/data/data_source/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const String baseUrl = "https://ezycourse.com/api/app";
+  static const String baseUrl = "https://ezyappteam.ezycourse.com/api/app";
 
   static const String login = "$baseUrl/student/auth/login";
 
